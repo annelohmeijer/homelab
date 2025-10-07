6135d629ff48bc9495dfc30f7e0818593a38d771
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2025-10-07 21:37:13 +0200

    gst

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+resource "hcloud_ssh_key" "default" {
+  name       = "k3s-key"
+  public_key = file("~/.ssh/homelab.pub")
+}
+
+resource "hcloud_network" "homelab" {
+  name     = "homelab-network"
+  ip_range = "10.0.0.0/16"
+
+}
+resource "hcloud_server" "homelab" {
+  name        = "homelab"
+  server_type = "cx22"
+  image       = "ubuntu-22.04"
+  location    = "nbg1" # Nuremberg
+  ssh_keys    = [hcloud_ssh_key.default.id]
+
+  labels = {
+    role = "server"
+  }
+}
+
+output "server_public_ip" {
+  value = hcloud_server.homelab.ipv4_address
+
+}
