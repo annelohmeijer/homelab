d4e4f3dbb1997373f6013c1b27e1bc15a2fd1ba0
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2025-10-15 20:50:43 +0200

    Add subnet :

--- infra/main.tf
+++ infra/main.tf
@@ -7,8 +7,15 @@ resource "hcloud_network" "homelab" {
   name = "homelab-network"
   # this provides 65K addresses (2 ^ 16 - 2)
   ip_range = "10.0.0.0/16"
-
 }
+
+resource "hcloud_network_subnet" "subnet" {
+  network_id   = hcloud_network.homelab.id
+  type         = "cloud"
+  ip_range     = "10.0.0.1/24"
+  network_zone = "eu-central"
+}
+
 resource "hcloud_server" "homelab" {
   name        = "homelab"
   server_type = "cx22"
